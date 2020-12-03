76a4e6936600f33d4366c18d10d16cf5ca1f9b70
bbanghao <bbanghao@naver.com>
2020-12-03 13:50:27 +0800

    Add: side length variable 'd'.
    t.forward(d)  #variable length

--- day06B-circle.py
+++ day06B-circle.py
@@ -1,11 +1,12 @@
 import turtle as t
 
 n=5		# 오각형
+d=100	# 변의 길이
 t.shape("turtle")
 t.color("purple")
 t.begin_fill()
 for x in range(n):
-	t.forward(50)
+	t.forward(d)
 	t.left(360/n)
 t.end_fill()
 
